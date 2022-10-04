dbfdc12f9f9ab7ab89ec25206d25c20ca3951255
KevinZMa <66984843+KevinZMa@users.noreply.github.com>
2022-10-04 15:00:48 -0700

    chapter 1 exercises

---
+++ ch1/ASCIIName.java
@@ -0,0 +1,12 @@
+public class ASCIIName {
+    public static void main(String[] args) {
+        System.out.println(
+              "*  * **** *   *  *  *   *\n"
+            + "* *  *    *   *  *  **  *\n"
+            + "**   **** *   *  *  * * *\n"
+            + "* *  *     * *   *  *  **\n"
+            + "*  * ****   *    *  *   *"
+        );
+    }
+}
+
---
+++ ch1/CowHelloWorld.java
@@ -0,0 +1,14 @@
+public class CowHelloWorld {
+    public static void main(String[] args) {
+        System.out.println(
+              " _____________ \n"
+            + "< hello world >\n"
+            + " ------------- \n"
+            + "        \\   ^__^\n"
+            + "         \\  (oo)\\_______\n"
+            + "            (__)\\       )\\/\n"
+            + "                ||----w |\n"
+            + "                ||     ||\n"
+        );
+    }
+}
