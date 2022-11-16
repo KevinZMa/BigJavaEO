c3d1f75835a010f28d77a341acb4d2dae083475d
KevinZMa <66984843+KevinZMa@users.noreply.github.com>
2022-11-16 08:57:04 -0800

    cash register (3.5)

---
+++ src/ch03/README.md
@@ -0,0 +1,7 @@
+# Chapter 3: Implementing Classes
+
+### Section 3.5 - [`cashregister/`](./cashregister/)
+
+**Problem Solving: Tracing Objects**
+
+The `CashRegister` class totals up sales and computes change due. 
---
+++ src/ch03/cashregister/CashRegisterTester.java
@@ -0,0 +1,21 @@
+package ch03.cashregister;
+
+/**
+ * A class to test the CashRegister class.
+ */
+public class CashRegisterTester {
+
+    public static void main(String[] args) {
+        // 9% sales tax on taxable purchases
+        CashRegister register = new CashRegister(9);
+
+        register.recordPurchase(30);
+        register.recordTaxablePurchase(20);
+        register.receivePayment(60);
+
+        double change = register.giveChange();
+
+        System.out.println(change);
+        System.out.println("Expected: 8.2");
+    }
+}
