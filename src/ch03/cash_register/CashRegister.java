16553382574b7afd3c8e02bbdde64ad04dc76892
KevinZMa <66984843+KevinZMa@users.noreply.github.com>
2022-12-18 19:53:22 -0800

    programming project P3.10

--- src/ch03/cash_register/CashRegister.java
+++ src/ch03/cash_register/CashRegister.java
@@ -10,38 +10,63 @@ public class CashRegister {
     private double taxablePurchase;
     private double payment;
     private int itemCount;
+    private String receipt = "";
+    private final String CURRENCY_SYMBOL = "$";
+
+    /**
+     * Constructs a cash register with no money in it and no tax rate.
+     */
+    public CashRegister() {
+        taxRate = 0.0;
+    }
 
     /**
      * Constructs a cash register with no money in it.
+     *
      * @param rate the tax rate for taxable purchases
      */
     public CashRegister(double rate) {
         taxRate = rate;
-        purchase = 0;
-        payment = 0;
+    }
+
+    /**
+     * Formats an amount of money to two decimal places.
+     * @param amount
+     * @return the formatted string
+     */
+    private String formatAmount(double amount) {
+        return String.format("%s%.2f", CURRENCY_SYMBOL, amount);
     }
 
     /**
      * Records the sale of a tax-free item.
+     *
      * @param amount the price of the item
      */
     public void recordPurchase(double amount) {
         purchase = purchase + amount;
         itemCount++;
+
+        if (!receipt.isEmpty()) receipt += "\n";
+        receipt += formatAmount(amount);
     }
 
     /**
      * P3.2 Records the sale of a taxable item.
+     *
      * @param amount the price of the item
      */
     public void recordTaxablePurchase(double amount) {
-        purchase = purchase + amount;
         taxablePurchase = taxablePurchase + amount;
         itemCount++;
+
+        if (!receipt.isEmpty()) receipt += "\n";
+        receipt += formatAmount(amount) + " (taxable)";
     }
 
     /**
      * Processes a payment received from the customer.
+     *
      * @param amount the amount of the payment
      */
     public void receivePayment(double amount) {
@@ -50,6 +75,7 @@ public class CashRegister {
 
     /**
      * Get total amount paid.
+     *
      * @return the amount of the payment
      */
     public double getPayment() {
@@ -57,15 +83,17 @@ public class CashRegister {
     }
 
     /**
-     * Get amount purchased.
+     * Get the total amount purchased, excluding tax.
+     *
      * @return the amount purchased
      */
     public double getPurchase() {
-        return purchase;
+        return purchase + taxablePurchase;
     }
 
     /**
      * Processes the total amount of sales tax due.
+     *
      * @return the amount of tax due
      */
     public double getTotalTax() {
@@ -74,6 +102,7 @@ public class CashRegister {
 
     /**
      * P3.1 Gets the amount of items purchased.
+     *
      * @return amount of items purchased
      */
     public int getItemCount() {
@@ -81,11 +110,31 @@ public class CashRegister {
     }
 
     /**
+     * P3.10 Returns the receipt of the prices of all purchased items and the total
+     * amount due.
+     *
+     * @return the receipt
+     */
+    public String getReceipt() {
+        if (receipt.isBlank()) return "Amount Due: " + CURRENCY_SYMBOL + "0.00";
+
+        String receipt = this.receipt + "\n";
+
+        double tax = getTotalTax();
+        if (tax > 0) receipt += "\nTotal Tax: " + formatAmount(tax);
+
+        receipt += "\nAmount Due: " + formatAmount(getPurchase() + tax);
+
+        return receipt;
+    }
+
+    /**
      * Computes the change due and resets the machine for the next customer.
+     *
      * @return the change due to the customer
      */
     public double giveChange() {
-        double change = payment - purchase - getTotalTax();
+        double change = payment - getPurchase() - getTotalTax();
         purchase = 0;
         taxablePurchase = 0;
         payment = 0;
--- src/ch03/cash_register/CashRegisterTester.java
+++ src/ch03/cash_register/CashRegisterTester.java
@@ -12,12 +12,36 @@ public class CashRegisterTester {
         register.recordPurchase(30);
         register.recordTaxablePurchase(20);
 
+        expect(register.getPurchase(), 50.0);
+
+        String expectedReceipt =
+            "$30.00\n$20.00 (taxable)\n\nTotal Tax: $1.80\nAmount Due: $51.80";
+        expect(register.getReceipt(), expectedReceipt);
+
         register.receivePayment(60);
 
-        System.out.println(register.getItemCount());
-        System.out.println("Expected: 2");
+        expect(register.getItemCount(), 2);
+        expect(register.giveChange(), 8.2);
+    }
 
-        System.out.println(register.giveChange());
-        System.out.println("Expected: 8.2");
+    private static void expect(Object resultObject, Object expectedObject) {
+        // cast to string
+        String result = String.valueOf(resultObject);
+        String expected = String.valueOf(expectedObject);
+
+        // TODO: inconsistencies with newlines across operating systems
+        if (result.contains("\n") || expected.contains("\n")) {
+            System.out.println("Received: ");
+            printWithNewlines(result);
+            System.out.println("Expected: ");
+            printWithNewlines(expected);
+        } else {
+            System.out.println("Received: " + result);
+            System.out.println("Expected: " + expected);
+        }
+    }
+
+    private static void printWithNewlines(String str) {
+        System.out.printf("\"\"\"%n%s%n\"\"\"%n", str);
     }
 }
